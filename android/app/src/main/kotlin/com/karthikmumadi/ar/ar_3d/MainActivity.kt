471717d0d6d5db21a020422db4ce3b1abbd84fb2
karthik Mumadi <karthikmumadi@karthiks-MacBook-Air.local>
2023-04-27 20:04:29 +0530

    AR 3D

---
+++ android/app/src/main/kotlin/com/karthikmumadi/ar/ar_3d/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.karthikmumadi.ar.ar_3d
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:ar_3d/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'AR APP',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
